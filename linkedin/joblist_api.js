74ff5438f72328149831f2be04b7a7b412c32d3b
Yugin Dimitriy <78262135+yugin-pro@users.noreply.github.com>
2024-11-17 12:38:48 +0100

    fix q method

--- linkedin/joblist_api.js
+++ linkedin/joblist_api.js
@@ -14,26 +14,26 @@ const voyagerJobsDashJobCards = {
       return `${projectEndpoint}linkedin/voyagerJobsDashJobCards/${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}/${categoryName}.json`},
     request:[],  
     runApi:async function() {
-      this.request[0] = this.apiUrl()
-      let res = await fetch(...this.request)
-      if(res.ok) {
+        this.request[0] = this.apiUrl()
+        let res = await fetch(...this.request)
+        if(res.ok) {
         let data = await res.json()
         return data
-      }
-      throw new Error(res.statusText)
+        }
+        throw new Error(res.statusText)
     },
     formCredentails: function(...reqParams) {
-      this.request = reqParams
-      let collection = Number(reqParams[0].match(/JobSearchCardsCollection-(\d+)\&/))
-      if (typeof collection === 'number' && collection > 10) {
+        this.request = reqParams
+        let collection = Number(reqParams[0].match(/JobSearchCardsCollection-(\d+)\&/))
+        if (typeof collection === 'number' && collection > 10) {
         this.collection = reqParams[0].match(/JobSearchCardsCollection-(\d+)\&/)
-      }
-      
-      return this
+        }
+        
+        return this
     },
     formPayload: async function() {
-      let apiResponse = await this.runApi()
-      return { 
+        let apiResponse = await this.runApi()
+        return { 
         ts: Date.now(),
         request: this.request[0],
         data: apiResponse.data,
@@ -41,53 +41,50 @@ const voyagerJobsDashJobCards = {
          }
     },
     post: async function() {
-      let payload = await this.formPayload()
-      let dataStr = JSON.stringify(payload)
-      let res = await fetch(this.url(this.keyword, this.firebaseProject),{method:'POST',body: dataStr.replace(/\$/g,'')})
-      let data = await res.json()
-      if (payload.data.paging)
-      this.start += 25
-      console.log(data, this.keyword)
+        let payload = await this.formPayload()
+        let dataStr = JSON.stringify(payload)
+        let res = await fetch(this.url(this.keyword, this.firebaseProject),{method:'POST',body: dataStr.replace(/\$/g,'')})
+        let data = await res.json()
+        if (payload.data.paging)
+        this.start += 25
+        console.log(data, this.keyword)
     },
     download:async function() {
-      this.start = 0
-      let d = await this.formPayload()
-      while (this.start < d.data.paging.total) {
+        this.start = 0
+        let d = await this.formPayload()
+        while (this.start < d.data.paging.total) {
         await this.post()
-      } 
-      console.log('download finished')
-      this.start = 0
-      return {status: 'finished'}
+        } 
+        console.log('download finished')
+        this.start = 0
+        return {status: 'finished'}
     },
     setKeyword: function(keyword) {     
-      this.keyword = encodeURIComponent(keyword)
-      return this
+        this.keyword = encodeURIComponent(keyword)
+        return this
     },
     setConfig(config) {
         const {project,fetchParams,keywordList} = config
         
         this.firebaseProject = project
-
+        
         this.request = fetchParams
         let collection = Number(fetchParams[0].match(/JobSearchCardsCollection-(\d+)\&/))
         if (typeof collection === 'number' && collection > 10) {
         this.collection = reqParams[0].match(/JobSearchCardsCollection-(\d+)\&/)
         }
-
+        
         this.keywordList = keywordList
         
         return this
     },
     createQueue() {
-        const apiLink = this
-       return function () {
-          let data = apiLink.keywordList[Symbol.iterator]()
-          return {
-            snap() {
-              let res = data.next().value 
-              return res ? apiLink.setKeyword(res)  : new Error('No value') 
-            }
-          }
+        let data = this.keywordList[Symbol.iterator]()
+        return {
+        snap() {
+          let res = data.next().value 
+          return res ? this.setKeyword(res)  : new Error('No value') 
+        }
         }
     }
   } 
